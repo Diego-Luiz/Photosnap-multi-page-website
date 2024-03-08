796d870aff775b61c65f51ccb982161eba63ecac
Diego Luiz <diegoreke@hotmail.com>
2024-03-08 15:53:06 -0400

    Add aria value changes according to header navbar

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const headerNav = (() => {
   const closeBtnImg = document.querySelector('.top-header .close-btn img');
   const navContainer = document.querySelector('.nav-container');
   const headerNav = document.getElementById('header-nav');
-  
+ 
   closeBtn.addEventListener('click', () => {
     navContainer.classList.toggle('--visible');
-    headerNav.toggleAttribute('aria-hidden');
-    headerNav.toggleAttribute('aria-expanded');
     headerNav.classList.toggle('--expanded');
+    headerNav.setAttribute('aria-hidden', headerNav.getAttribute('aria-hidden') == 'true' ? false : true);
+    headerNav.setAttribute('aria-expanded', headerNav.getAttribute('aria-expanded') == 'true' ? false : true);
     closeBtn.classList.add('--changeStatus');
     setTimeout(() => {closeBtn.classList.remove('--changeStatus');}, 500);
     if(navContainer.classList.contains('--visible')) closeBtnImg.src = "./assets/images/shared/mobile/close.svg";
