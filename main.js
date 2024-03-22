2d92963819159f1f4f8aa4f331755b53df076d5c
Diego Luiz <diegoreke@hotmail.com>
2024-03-22 16:03:17 -0400

    Add price dynamic changes via js & css

--- main.js
+++ main.js
@@ -15,3 +15,29 @@ const headerNav = (() => {
     else closeBtnImg.src = "./assets/images/shared/mobile/menu.svg";
   });
 })();
+
+const pricingPlans = (() => {
+  const pricingForm = document.getElementById('pricing-form');
+  const inputContainer = document.getElementsByClassName('input-container')[0];
+  const toggleBtn = document.getElementsByClassName('toggle-btn')[0];
+  const typeOfPlan = document.getElementsByName('type-of-plan');
+  const plansAvailable = document.querySelector('.plans-available');
+  pricingForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+  });
+  inputContainer.addEventListener('click', (event) => {
+    if(event.target == inputContainer || event.target == toggleBtn) {
+      toggleBtn.classList.toggle('--toggle');
+      typeOfPlan.forEach((input) => {
+        input.toggleAttribute('checked');
+      })
+      if(plansAvailable.classList.contains('monthly')) {
+        plansAvailable.classList.remove('monthly');
+        plansAvailable.classList.add('yearly');
+      } else {
+        plansAvailable.classList.remove('yearly');
+        plansAvailable.classList.add('monthly');
+      }
+    }
+  });
+})();
